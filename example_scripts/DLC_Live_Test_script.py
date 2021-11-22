a80acf8a51034f989abe620964307452420d72ea
Fabian Plum <fabianplum@web.de>
2021-11-22 13:52:42 +0000

    Added DLC test script

---
+++ example_scripts/DLC_Live_Test_script.py
@@ -0,0 +1,43 @@
+from dlclive import DLCLive, Processor
+import cv2
+
+dlc_proc = Processor()
+model_path = "I:/FARTS/DeepLabCut-Multi-Animal/multi_ant_3_class_FARTS-2021-11-07/exported-models/DLC_multi_ant_test_label_resnet_50_iteration-0_shuffle-1"
+dlc_live = DLCLive(model_path, processor=dlc_proc, dynamic=(False, 0.1, 10))
+
+video_path = "C:/Users/PlumStation/Desktop/OmniTrax_Testing/single_ant_1080p.mp4"
+cap = cv2.VideoCapture(video_path)
+
+max_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+frame_num = 0
+print(max_frames)
+
+plot_IDs = [0,1,2,3,4,5,6]#,8,9,15,16,22,23,29,30,36,37,43,44]
+
+while(cap.isOpened()):
+    
+    frame_num += 1
+    if frame_num == max_frames:
+        break
+    
+    ret, frame = cap.read()
+    try:
+        if ret == True:
+            if frame_num == 1:
+                dlc_live.init_inference(frame)
+                
+            pose = dlc_live.get_pose(frame)
+            print(pose.shape)
+            
+            for p, point in enumerate(pose):
+                if p in plot_IDs:
+                    frame  = cv2.circle(frame ,(int(point[0]),int(point[1])), 5, (int(255 * point[2]), int(100 * point[2]),200), -1)
+                    
+            cv2.imshow("DLC-Live_output",frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    except:
+        print("Yeah, that's enough for now.")
+  
+cv2.destroyAllWindows() 
+print("Read all frames")
