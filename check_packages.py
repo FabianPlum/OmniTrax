7ed7643b48dca81dd905bea37ba05353447600db
Fabian Plum <fabianplum@web.de>
2022-11-02 13:52:41 +0000

    Improved package installation and dependency checks upon launching blender

--- check_packages.py
+++ check_packages.py
@@ -1,74 +1,109 @@
 import subprocess
 import sys
 from pathlib import Path
+import os
+
+script_file = os.path.realpath(__file__)
+directory = os.path.dirname(script_file)
 
 print("\nINFO: Checking requirements for omni_trax addon...\n")
 
-# get path of blender internal python executable
-py_exec = str(sys.executable)
+# only run the following checks upon initial installation
+try:
+    with open(os.path.join(directory, "setup_state.txt")) as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.rstrip('\n')
+            line_elems = line.split("=")
+            if line_elems[0] == "setup_complete":
+                if line_elems[1] == "True":
+                    setup_complete = True
+                else:
+                    setup_complete = False
+except FileNotFoundError:
+    setup_complete = False
 
-# install packages (if they are not already installed)
-required_libraries = {"scipy": "scipy",
-                      "pandas": "pandas",
-                      "PyYAML": "PyYAML",
-                      "matplotlib": "matplotlib",
-                      "opencv-python": "opencv-python",
-                      "scikit-learn": "scikit-learn",
-                      "deeplabcut-live": "deeplabcut-live"}
+if not setup_complete:
+    first_pkg = True
+    setup_state_f_contents = []
+    # get path of blender internal python executable
+    py_exec = str(sys.executable)
 
-reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
-installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+    # install packages (if they are not already installed)
+    required_libraries = {"scipy": "scipy",
+                          "pandas": "pandas",
+                          "PyYAML": "PyYAML",
+                          "matplotlib": "matplotlib",
+                          "opencv-python": "opencv-python",
+                          "scikit-learn": "scikit-learn",
+                          "deeplabcut-live": "deeplabcut-live"}
 
-print("\nInstalled packages:", installed_packages, "\n")
+    reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+    installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
 
-first_check = True
+    print("\nInstalled packages:", installed_packages, "\n")
 
-for library in required_libraries:
-    if library in installed_packages:
-        print(f"{library!r} already installed!")
-    else:
-        print(f"{library!r} not found! Installing package...\n")
-        if first_check:
-            # ensure pip is installed
-            subprocess.call([py_exec, "-m", "ensurepip", "--user"])
-            # update pip
-            subprocess.call([py_exec, "-m", "pip", "install", "--upgrade", "pip"])
-        subprocess.call(
-            [py_exec, "-m", "pip", "install", f"--target={py_exec[:-14]}" + "lib", required_libraries[library]])
+    for library in required_libraries:
+        if library in installed_packages:
+            print(f"{library!r} already installed!")
+        else:
+            print(f"{library!r} not found! Installing package...\n")
+            if first_pkg:
+                # ensure pip is installed
+                subprocess.call([py_exec, "-m", "ensurepip", "--user"])
+                # update pip
+                subprocess.call([py_exec, "-m", "pip", "install", "--upgrade", "pip"])
+                first_pkg = False
 
-# Okay, this next part is hacky AF, but, for now, it seems like the only way to load DLC-Live...
-# We need to suppress loading tkinter... So let's overwrite the "display.py" file within DLC-live.
+            subprocess.call(
+                [py_exec, "-m", "pip", "install", f"--target={py_exec[:-14]}" + "lib", required_libraries[library]])
 
-# Cool. Here goes nothing
+        setup_state_f_contents.append(library + "=True")
 
-OVERWRITE_DLC_DISPLAY = False
-dlclive_display = Path.joinpath(Path(py_exec[:-14]), "lib", "dlclive", "display.py")
+    # Okay, this next part is hacky AF, but, for now, it seems like the only way to load DLC-Live...
+    # We need to suppress loading tkinter... So let's overwrite the "display.py" file within DLC-live.
 
-orig_tk_import = "from tkinter import Tk, Label\n"
-orig_PIL_import = "from PIL import Image, ImageTk, ImageDraw\n"
+    # Cool. Here goes nothing
 
-updated_tk_import = "# " + orig_tk_import
-updated_PIL_import = "# " + orig_PIL_import
+    OVERWRITE_DLC_DISPLAY = False
+    dlclive_display = Path.joinpath(Path(py_exec[:-14]), "lib", "dlclive", "display.py")
 
-dlclive_display_file = open(str(dlclive_display), "r")
-updated_file_content = ""
+    orig_tk_import = "from tkinter import Tk, Label\n"
+    orig_PIL_import = "from PIL import Image, ImageTk, ImageDraw\n"
 
-for line in dlclive_display_file:
-    if line == orig_tk_import:
-        line = updated_tk_import
-        OVERWRITE_DLC_DISPLAY = True
-    if line == orig_PIL_import:
-        line = updated_PIL_import
-        OVERWRITE_DLC_DISPLAY = True
+    updated_tk_import = "# " + orig_tk_import
+    updated_PIL_import = "# " + orig_PIL_import
 
-    updated_file_content += line
+    dlclive_display_file = open(str(dlclive_display), "r")
+    updated_file_content = ""
 
-dlclive_display_file.close()
+    for line in dlclive_display_file:
+        if line == orig_tk_import:
+            line = updated_tk_import
+            OVERWRITE_DLC_DISPLAY = True
+        if line == orig_PIL_import:
+            line = updated_PIL_import
+            OVERWRITE_DLC_DISPLAY = True
 
-if OVERWRITE_DLC_DISPLAY:
-    dlclive_display_file = open(str(dlclive_display), "w")
-    dlclive_display_file.write(updated_file_content)
-    print("\nINFO: /dlclive/display.py has been updated to suppress tkinter import!!!")
+        updated_file_content += line
+
     dlclive_display_file.close()
 
-print("\nINFO: Cool, all looks good here! \nINFO: Uncheck OmniTrax in your addons to remove checks at program start!")
+    if OVERWRITE_DLC_DISPLAY:
+        dlclive_display_file = open(str(dlclive_display), "w")
+        dlclive_display_file.write(updated_file_content)
+        print("\nINFO: /dlclive/display.py has been updated to suppress tkinter import!!!")
+        dlclive_display_file.close()
+
+    setup_state_f_contents.append("overwritten_orig_dlclive=True")
+    setup_state_f_contents.append("setup_complete=True")
+
+    with open(os.path.join(directory, "setup_state.txt"), 'w') as f:
+        for line in setup_state_f_contents:
+            f.write(line)
+            f.write('\n')
+
+    print(
+        "\nINFO: Cool, all looks good here! \nINFO: Uncheck OmniTrax in your addons to remove checks at program start!")
+else:
+    print("\nINFO: successfully loaded OmniTrax")
