88a637a420a234d8b3d6451f9599a96746442450
Fabian Plum <fabianplum@web.de>
2022-11-08 23:27:43 +0000

    Added tracker + KF unit test

--- tests/tracker.test.py
+++ tests/tracker.test.py
@@ -86,6 +86,38 @@ try:
             self.assertIsNone(np.testing.assert_array_equal(tracker.tracks[0].trace[-1], target_2[0]))
             self.assertIsNone(np.testing.assert_array_equal(tracker.tracks[1].trace[-1], target_2[1]))
 
+        def test_update_KF(self):
+            dist_thresh_1 = 600
+            detections_1 = [np.array([[100], [200]]),
+                            np.array([[300], [400]])]
+            detections_2 = [np.array([[300], [400]]),
+                            np.array([[500], [600]])]
+            target_1 = [np.array([[295], [395]]),
+                        np.array([[495], [595]])]
+            bbox = [0, 0, 0, 0]
+
+            tracker = Tracker(dist_thresh=dist_thresh_1,
+                              max_frames_to_skip=2,
+                              max_trace_length=10,
+                              trackIdCount=0,
+                              use_kf=True,
+                              std_acc=10,
+                              x_std_meas=1,
+                              y_std_meas=1,
+                              dt=1)
+
+            tracker.Update(detections=detections_1,
+                           predicted_classes=[0, 0],
+                           bounding_boxes=[bbox, bbox])
+
+            tracker.Update(detections=detections_2,
+                           predicted_classes=[0, 0],
+                           bounding_boxes=[bbox, bbox])
+
+            self.assertEqual(len(tracker.tracks), 2)
+            self.assertIsNone(np.testing.assert_array_equal(tracker.tracks[0].trace[-1], target_1[0]))
+            self.assertIsNone(np.testing.assert_array_equal(tracker.tracks[1].trace[-1], target_1[1]))
+
 
     suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestTracker)
     success = unittest.TextTestRunner().run(suite)
